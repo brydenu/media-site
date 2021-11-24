f04cca073dc7350669a29a887e45750c6741cd00
Bryden Uyehara <brydenu@gmail.com>
2021-11-23 20:09:13 -0800

    updated app and index to reflex the content of the application, and to make use of BrowserRouter from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./Common/Navbar";
+import Routing from "./Routes/Routing";
+import "./Styles/App.css";
 
 export default function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar />
+                <Routing />
+            </BrowserRouter>
+        </div>
+    );
 }
